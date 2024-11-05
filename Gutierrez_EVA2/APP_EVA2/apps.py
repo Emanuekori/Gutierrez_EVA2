d0a47e5f3d3578db90b9050694152e6875ae7c06
Emanuel Gutiérrez <emanuek3@gmail.com>
2024-11-05 19:27:03 -0300

    backend

---
+++ Gutierrez_EVA2/APP_EVA2/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppEva2Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'APP_EVA2'
---
+++ Gutierrez_EVA2/APP_EVA2/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from .models import Inscripcion
+from .forms import InscripcionForm
+
+# Listar inscripciones
+def listar_inscripciones(request):
+    inscripciones = Inscripcion.objects.all()
+    return render(request, 'listar_inscripciones.html', {'inscripciones': inscripciones})
+
+# Agregar inscripción
+def agregar_inscripcion(request):
+    if request.method == 'POST':
+        form = InscripcionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_inscripciones')
+    else:
+        form = InscripcionForm()
+    return render(request, 'agregar_inscripcion.html', {'form': form})
+
+# Modificar inscripción
+def modificar_inscripcion(request, id):
+    inscripcion = Inscripcion.objects.get(id=id) 
+    if request.method == 'POST':
+        form = InscripcionForm(request.POST, instance=inscripcion)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_inscripciones')
+    else:
+        form = InscripcionForm(instance=inscripcion)
+    return render(request, 'agregar_inscripcion.html', {'form': form})
+
+# Eliminar inscripción
+def eliminar_inscripcion(request, id):
+    inscripcion = Inscripcion.objects.get(id=id)  
+    if request.method == 'POST':
+        inscripcion.delete()
+        return redirect('listar_inscripciones')
+    return render(request, 'confirmar_eliminacion.html', {'inscripcion': inscripcion})
